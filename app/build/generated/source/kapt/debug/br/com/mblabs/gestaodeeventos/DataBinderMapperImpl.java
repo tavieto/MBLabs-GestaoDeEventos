b8645920b2c0459a21f42087650efbe6694d8d36
Otávio Gabriel <tavieto@outlook.com>
2021-08-18 20:48:23 -0300

    add databinding

--- app/build/generated/source/kapt/debug/br/com/mblabs/gestaodeeventos/DataBinderMapperImpl.java
+++ app/build/generated/source/kapt/debug/br/com/mblabs/gestaodeeventos/DataBinderMapperImpl.java
@@ -6,6 +6,8 @@ import android.view.View;
 import androidx.databinding.DataBinderMapper;
 import androidx.databinding.DataBindingComponent;
 import androidx.databinding.ViewDataBinding;
+import br.com.mblabs.gestaodeeventos.databinding.ActivityMainBindingImpl;
+import java.lang.IllegalArgumentException;
 import java.lang.Integer;
 import java.lang.Object;
 import java.lang.Override;
@@ -16,7 +18,13 @@ import java.util.HashMap;
 import java.util.List;
 
 public class DataBinderMapperImpl extends DataBinderMapper {
-  private static final SparseIntArray INTERNAL_LAYOUT_ID_LOOKUP = new SparseIntArray(0);
+  private static final int LAYOUT_ACTIVITYMAIN = 1;
+
+  private static final SparseIntArray INTERNAL_LAYOUT_ID_LOOKUP = new SparseIntArray(1);
+
+  static {
+    INTERNAL_LAYOUT_ID_LOOKUP.put(br.com.mblabs.gestaodeeventos.R.layout.activity_main, LAYOUT_ACTIVITYMAIN);
+  }
 
   @Override
   public ViewDataBinding getDataBinder(DataBindingComponent component, View view, int layoutId) {
@@ -25,6 +33,14 @@ public class DataBinderMapperImpl extends DataBinderMapper {
       final Object tag = view.getTag();
       if(tag == null) {
         throw new RuntimeException("view must have a tag");
+      }
+      switch(localizedLayoutId) {
+        case  LAYOUT_ACTIVITYMAIN: {
+          if ("layout/activity_main_0".equals(tag)) {
+            return new ActivityMainBindingImpl(component, view);
+          }
+          throw new IllegalArgumentException("The tag for activity_main is invalid. Received: " + tag);
+        }
       }
     }
     return null;
@@ -78,6 +94,10 @@ public class DataBinderMapperImpl extends DataBinderMapper {
   }
 
   private static class InnerLayoutIdLookup {
-    static final HashMap<String, Integer> sKeys = new HashMap<String, Integer>(0);
+    static final HashMap<String, Integer> sKeys = new HashMap<String, Integer>(1);
+
+    static {
+      sKeys.put("layout/activity_main_0", br.com.mblabs.gestaodeeventos.R.layout.activity_main);
+    }
   }
 }
