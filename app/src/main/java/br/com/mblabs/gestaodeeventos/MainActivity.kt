5f8235c2643a0d363b2ab91e3bc0def7795d66e9
Otávio Gabriel <tavieto@outlook.com>
2021-08-18 20:50:24 -0300

    add viewModel and remove business rules from view

--- app/src/main/java/br/com/mblabs/gestaodeeventos/MainActivity.kt
+++ app/src/main/java/br/com/mblabs/gestaodeeventos/MainActivity.kt
@@ -2,130 +2,60 @@ package br.com.mblabs.gestaodeeventos
 
 import android.os.Bundle
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.android.gms.common.SignInButton
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.ValueEventListener
-import com.google.firebase.database.ktx.database
-import com.google.firebase.database.ktx.getValue
-import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
+import androidx.databinding.DataBindingUtil
+import br.com.mblabs.gestaodeeventos.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var uid: String
-    private lateinit var email: String
-
-    private val signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract()
-    ) { res ->
-            this.onSignInResult(res)
-        }
+    private lateinit var binding: ActivityMainBinding
+    private val viewModel: MainViewModel by lazy {
+        MainViewModel(activity = this)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val user = FirebaseAuth.getInstance().currentUser
-        setEmail(user?.email ?: "")
-        setFirebaseAuthentication()
-        findViewById<AppCompatButton>(R.id.btnDB).setOnClickListener {
-            setFirebaseRealTimeDatabase()
-        }
-        findViewById<AppCompatButton>(R.id.btnDBGET).setOnClickListener {
-            readDB()
-        }
-        findViewById<AppCompatButton>(R.id.btnLogout).setOnClickListener {
-            AuthUI.getInstance().signOut(this).addOnCompleteListener {
+        configureView()
+        setClickListeners()
+        setObservers()
+    }
+
+    private fun setObservers() {
+        viewModel.emailIsNotEmpty.observe(this, {
+            if (it) {
                 setEmail()
             }
-        }
-    }
-
-    private fun setFirebaseAuthentication() {
-        // Choose authentication provider
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.GoogleBuilder().build()
-        )
-
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-
-        val googleButton = findViewById<SignInButton>(R.id.btnLogin)
-
-        val textView: TextView = googleButton.getChildAt(0) as TextView
-        textView.setText(R.string.sign_in_google)
-
-        googleButton.setOnClickListener {
-            signInLauncher.launch(signInIntent)
-        }
-    }
-
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            user?.let {
-                Toast.makeText(this, "Deu certo e o e-mail do usuário é ${it.email}", Toast.LENGTH_SHORT).show()
-                uid = it.uid
-                email = it.email.toString()
-                setEmail(email)
-            }
-        } else {
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show()
-        }
-    }
-
-    private fun setEmail(email: String = "") {
-        findViewById<AppCompatTextView>(R.id.txtEmail).text = email
-    }
-
-    private fun setFirebaseRealTimeDatabase() {
-
-        val database = Firebase.database
-        val myRef = database.getReference(uid)
-        val user = User(email, uid)
-
-        val valueUser = Gson().toJson(user)
-
-        Toast.makeText(this@MainActivity, "$user", Toast.LENGTH_SHORT).show()
-        Toast.makeText(this@MainActivity, "$valueUser", Toast.LENGTH_SHORT).show()
-        myRef.setValue(valueUser)
-
-        readDB()
-    }
-
-    private fun readDB() {
-        val database = Firebase.database
-        val myRef = database.getReference(uid)
-        lateinit var json: User
-
-        myRef.addValueEventListener(object: ValueEventListener {
-            override fun onDataChange(snapshot: DataSnapshot) {
-                val valueDB = snapshot.getValue<String>()
-                valueDB?.let {
-                    json = Gson().fromJson(it, User::class.java)
-                }
-                Toast.makeText(this@MainActivity, "Value is: ${json.email}", Toast.LENGTH_SHORT).show()
-            }
-
-            override fun onCancelled(error: DatabaseError) {
-                Toast.makeText(this@MainActivity, "Failed to read value.", Toast.LENGTH_SHORT).show()
-            }
-
         })
     }
+
+    private fun setClickListeners() {
+        binding.btnLogin.setOnClickListener {
+            viewModel.setFirebaseAuthentication()
+        }
+        binding.btnDB.setOnClickListener {
+            viewModel.setFirebaseRealTimeDatabase()
+        }
+        binding.btnDBGET.setOnClickListener {
+            viewModel.readDB()
+        }
+        binding.btnLogout.setOnClickListener {
+            viewModel.logout()
+        }
+    }
+
+    private fun configureView() {
+        val signInGoogle: TextView = binding.btnLogin.getChildAt(0) as TextView
+        signInGoogle.setText(R.string.sign_in_google)
+
+        setEmail()
+    }
+
+    private fun setEmail() {
+        binding.txtEmail.text = viewModel.getEmail()
+    }
+
 
 }
---
+++ app/src/main/java/br/com/mblabs/gestaodeeventos/MainViewModel.kt
@@ -0,0 +1,97 @@
+package br.com.mblabs.gestaodeeventos
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.database.ktx.getValue
+import com.google.firebase.ktx.Firebase
+import com.google.gson.Gson
+
+class MainViewModel(private val activity: MainActivity): ViewModel() {
+
+    var currentUser = FirebaseAuth.getInstance().currentUser
+    private val database = Firebase.database
+    lateinit var myRef: DatabaseReference
+
+    private val _emailIsNotEmpty: MutableLiveData<Boolean> = MutableLiveData()
+    val emailIsNotEmpty: LiveData<Boolean> = _emailIsNotEmpty
+
+    private val signInLauncher = activity.registerForActivityResult(
+        FirebaseAuthUIActivityResultContract()
+    ) { res ->
+        onSignInResult(res)
+    }
+
+    fun setFirebaseAuthentication() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+
+        signInLauncher.launch(signInIntent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == AppCompatActivity.RESULT_OK) {
+            val user = FirebaseAuth.getInstance().currentUser
+            user?.let {
+                currentUser = it
+            }
+            _emailIsNotEmpty.value = true
+        }
+    }
+
+    fun setFirebaseRealTimeDatabase() {
+        currentUser?.let { firebaseUser ->
+            myRef = database.getReference(firebaseUser.uid)
+            val user = User(firebaseUser.email.toString(), firebaseUser.uid)
+            val userJson = Gson().toJson(user)
+            myRef.setValue(userJson)
+        }
+    }
+
+    fun readDB() {
+        currentUser?.let { firebaseUser ->
+            firebaseUser.uid.let {
+                myRef = database.getReference(it)
+            }
+        }
+        lateinit var json: User
+
+        myRef.addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val valueDB = snapshot.getValue<String>()
+                valueDB?.let {
+                    json = Gson().fromJson(it, User::class.java)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {}
+        })
+    }
+
+    fun logout() {
+        AuthUI.getInstance().signOut(activity).addOnCompleteListener {
+            _emailIsNotEmpty.value = false
+        }
+    }
+
+    fun getEmail(): String {
+        return currentUser?.email ?: ""
+    }
+}
