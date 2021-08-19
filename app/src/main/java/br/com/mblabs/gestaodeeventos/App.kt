2646d3947bfb42d38c2f1a2d09ad3b79d93fba8f
Otávio Gabriel <tavieto@outlook.com>
2021-08-19 05:15:13 -0300

    add timber

---
+++ app/src/main/java/br/com/mblabs/gestaodeeventos/App.kt
@@ -0,0 +1,12 @@
+package br.com.mblabs.gestaodeeventos
+
+import android.app.Application
+import timber.log.Timber
+
+class App: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Timber.plant(Timber.DebugTree())
+    }
+}
